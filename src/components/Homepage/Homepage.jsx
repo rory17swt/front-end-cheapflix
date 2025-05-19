e271c14bd5d36eb78a25fc688c516168fc9a3026
afole <koladeafo@gmail.com>
2025-05-19 17:38:23 +0100

    styling 1

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -35,7 +35,7 @@ export default function HomePage (){
                 <h1>CheapFlix</h1>
                 <p>premium low budget entertainment from the comfort of your screen</p>
             </div>
-             <div>
+             <div className='movieDisplay'>
                 <section className="movie-list">
                     {error 
                     ? <p className='error-message'>{error}</p>
@@ -43,21 +43,35 @@ export default function HomePage (){
                             ? <Spinner />
                             : movies.length > 0
                             ? 
-                                <section>
-                                    <div>
-                                        <img src={movies[index].movieImage} alt='movie cover'/>
-                                        <h1>{movies[index].title}</h1>
+                                <section className='displayMovie'>
+                                    <button onClick={back}>back</button>
+                                    <div className='singleDisplaymovie'>
+                                        <div className='image'>
+                                            <img src={movies[index].movieImage} alt='movie cover'/>
+                                        </div>
+                                        <Link key={movies[index]._id} to={`/movies/${movies[index]._id}`}><h1>{movies[index].title}</h1></Link>
+                                        <p>{movies[index].director}</p>
+                                        <p>{movies[index].runTime}</p>
                                     </div>
-                                     <div>
-                                        <img src={movies[index + 1].movieImage} alt='movie cover'/>
-                                        <h1>{movies[index + 1].title}</h1>
+                                     <div className='singleDisplaymovie'>
+                                        <div className='image'>
+                                            <img src={movies[index + 1].movieImage} alt='movie cover'/>
+                                        </div>
+                                        <Link key={movies[index + 1]._id} to={`/movies/${movies[index + 1]._id}`}><h1>{movies[index + 1].title}</h1></Link>
+                                        <p>{movies[index + 1].director}</p>
+                                        <p>{movies[index + 1].runTime}</p>
                                     </div>
-                                     <div>
-                                        <img src={movies[index + 2].movieImage} alt='movie cover'/>
-                                        <h1>{movies[index + 2].title}</h1>
+                                     <div className='singleDisplaymovie'>
+                                        <div className='image'>
+                                            <img src={movies[index + 2].movieImage} alt='movie cover'/>
+                                        </div>
+                                        
+                                        <Link key={movies[index + 2]._id} to={`/movies/${movies[index + 2]._id}`}><h1>{movies[index + 2].title}</h1></Link>
+                                        <p>{movies[index + 2].director}</p>
+                                        <p>{movies[index + 2].runTime}</p>
                                     </div>
                                     <button onClick={next}>next</button>
-                                    <button onClick={back}>back</button>
+                                    
                                 </section>
                                 
                                     : <h1>No movies on our raider</h1>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,16 +4,16 @@ import './Navbar.css'
 export default function Navbar(){
   return (
     <header>
-      <div className='brand-logo'>
-        <NavLink to="/"><img src="/" alt="/"/>🎥🍿</NavLink>
-      </div>
+      <nav className='brand-logo'>
+        <NavLink to="/"><img src="https://res.cloudinary.com/dmqk8mtwl/image/upload/v1747667874/Screenshot_2025-05-19_161721_qf1qop.png" alt="🎥🍿"/></NavLink>
+      </nav>
       <nav className="functions">
-        <NavLink to="/movies">movies</NavLink>
-        <NavLink to="/movies/new">Post your movie</NavLink>
+        <NavLink to="/movies" className= "nav-link">movies</NavLink>
+        <NavLink to="/movies/new" className= "nav-link">Post your movie</NavLink>
       </nav>
       <nav className="authentication">
-        <NavLink to="/register">create an account</NavLink>
-        <NavLink to="/signIn">signIn</NavLink>
+        <NavLink to="/register" className= "nav-link">create an account</NavLink>
+        <NavLink to="/signIn" className= "nav-link">signIn</NavLink>
       </nav>
     </header>
   )
