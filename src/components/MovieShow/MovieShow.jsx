9b7089b8b473df94e09a0b9bc316de1e4dd6ea4d
Charles Adams 19 <adamscra19@gmail.com>
2025-05-19 13:51:43 +0100

    comments on show page

--- src/components/MovieShow/MovieShow.jsx
+++ src/components/MovieShow/MovieShow.jsx
@@ -8,41 +8,59 @@ import { useContext } from 'react'
 import MovieDelete from '../MovieDelete/MovieDelete'
 import Spinner from '../Spinner/Spinner'
 
-export default function MovieShow(){
+export default function MovieShow() {
   // * Params
   const { movieId } = useParams()
 
   const { user } = useContext(UserContext)
 
   // * State
-  const { data: movie, isLoading, error } = useFetch(
+  const { data: response, isLoading, error } = useFetch(
     getSingleMovie,
-    {}, 
+    {},
     movieId
   )
-
-  return (
-    <>
-      {error
-        ? <p className='error-message'>{error}</p>
-        : isLoading
-          ? <Spinner />
-          : (
-            <section className="single-movie">
-              <img src = {movie.movieImage} alt='movie image'/>
-              <h1>{movie.title}</h1>
-              <p>{movie.director}</p>
-              <p>{movie.runTime}</p>
-              <p>{movie.tags}</p>
-              { user && user._id === movie.owner &&
-                <div className="controls">
-                  <Link className='edit-movie' to={`/movies/${movieId}/edit`}>Edit</Link>
-                  <MovieDelete />
-                </div>
-              }
-            </section>
-          )
-      }
-    </>
-  )
+  const { movie, comments } = response
 }
+
+ return (
+  <>
+    {error ? (
+      <p className='error-message'>{error}</p>
+    ) : isLoading ? (
+      <Spinner />
+    ) : (
+      <>
+        <section className="single-movie">
+          <img src={movie.movieImage} alt='movie image' />
+          <h1>{movie.title}</h1>
+          <p>{movie.director}</p>
+          <p>{movie.runTime}</p>
+          <p>{movie.tags}</p>
+          {user && user._id === movie.owner && (
+            <div className="controls">
+              <Link className='edit-movie' to={`/movies/${movieId}/edit`}>Edit</Link>
+              <MovieDelete />
+            </div>
+          )}
+        </section>
+
+        <section className="comments">
+          <h2>Comments</h2>
+          {comments.length === 0 ? (
+            <p>No comments yet.</p>
+          ) : (
+            <ul>
+              {comments.map(comment => (
+                <li key={comment._id}>
+                  <p>{comment.content}</p>
+                  <small>By: {comment.author?.username}</small>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
+      </>
+    )}
+  </>
+)
