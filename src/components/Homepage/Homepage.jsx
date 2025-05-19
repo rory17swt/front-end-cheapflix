e6c3b84376175440ae49cebd3255389e0e2e7d45
afole <koladeafo@gmail.com>
2025-05-19 15:38:43 +0100

    home 2

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,18 +1,25 @@
 import './Homepage.css'
 import { Link } from 'react-router'
+import { useState } from 'react'
 import useFetch from '../../hooks/useFetch'
 
 import { getAllMovies } from '../../services/movies'
 import Spinner from '../Spinner/Spinner'
-let index = 0
+
 
 
 export default function HomePage (){
     const { data: movies, isLoading, error } = useFetch(getAllMovies, [])
+    const [index, setIndex] = useState(0)
     
+    console.log(movies)
+
     const next = () => {
         if (index + 3 < movies.length) {
             setIndex(index + 3)
+        }
+        if (index + 3 === movies.length){
+            setIndex(0)
         }
     }
 
@@ -25,38 +32,39 @@ export default function HomePage (){
     return(
         <>
             <div className='slogan'>
-                <h1>CheapFlex</h1>
+                <h1>CheapFlix</h1>
                 <p>premium low budget entertainment from the comfort of your screen</p>
             </div>
-            {/* <div>
+             <div>
                 <section className="movie-list">
                     {error 
                     ? <p className='error-message'>{error}</p>
                         : isLoading
                             ? <Spinner />
                             : movies.length > 0
-                            ? movies.map(movie => (
-                                <Link key={movie._id} to={`/movies/${movie._id}`}>
+                            ? 
+                                <section>
                                     <div>
-                                        <img src={movie[index].movieImage} alt='movie cover'/>
-                                        <h1>{movie[index].title}</h1>
+                                        <img src={movies[index].movieImage} alt='movie cover'/>
+                                        <h1>{movies[index].title}</h1>
                                     </div>
                                      <div>
-                                        <img src={movie[index + 1].movieImage} alt='movie cover'/>
-                                        <h1>{movie[index + 1].title}</h1>
+                                        <img src={movies[index + 1].movieImage} alt='movie cover'/>
+                                        <h1>{movies[index + 1].title}</h1>
                                     </div>
                                      <div>
-                                        <img src={movie[index + 2].movieImage} alt='movie cover'/>
-                                        <h1>{movie[index + 2].title}</h1>
+                                        <img src={movies[index + 2].movieImage} alt='movie cover'/>
+                                        <h1>{movies[index + 2].title}</h1>
                                     </div>
                                     <button onClick={next}>next</button>
-                                </Link>
-                                    ))
+                                    <button onClick={back}>back</button>
+                                </section>
+                                
                                     : <h1>No movies on our raider</h1>
                                 }
                 </section>  
             </div>
-             */}
+            
         </>
     )
 }
