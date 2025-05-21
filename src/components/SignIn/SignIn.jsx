178cf73dfb26fb78f571744f1e0859859ca77478
Rory Swietlicki <rory@swietlicki.eu>
2025-05-21 19:07:40 +0100

    updated styling to signin page

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,11 +1,11 @@
 import './SignIn.css'
 import { Link, useNavigate, Navigate } from "react-router"
 import { useState, useContext } from 'react'
-import {signin} from "../../services/auth"
+import { signin } from "../../services/auth"
 import { setToken, getUserFromToken } from "../../utils/auth"
-import {UserContext} from '../../contexts/UserContext'
+import { UserContext } from '../../contexts/UserContext'
 
-export default function     SignIn(){
+export default function SignIn() {
   // * Context
   const { user, setUser } = useContext(UserContext)
 
@@ -21,55 +21,55 @@ export default function     SignIn(){
   const navigate = useNavigate()
 
   // * Functions
-  const handleChange = ({ target: { name, value }}) => {
+  const handleChange = ({ target: { name, value } }) => {
     setFormData({ ...formData, [name]: value })
     setError({ ...error, [name]: '' })
   }
 
-const handleSubmit = async (evt) => {
+  const handleSubmit = async (evt) => {
     evt.preventDefault()
     setIsLoading(true)
     try {
-const { data } = await signin(formData) 
-setToken(data.token) 
-setUser(getUserFromToken())
-// to check 1 
-navigate('/movies')
+      const { data } = await signin(formData)
+      setToken(data.token)
+      setUser(getUserFromToken())
+      // to check 1 
+      navigate('/movies')
     } catch (error) {
-        setError(error.response.data)
+      setError(error.response.data)
     } finally {
-        setIsLoading(false)
+      setIsLoading(false)
     }
-}
-// if (user) {
-//     return <Navigate to="/" />
-// }
- return (
+  }
+  // if (user) {
+  //     return <Navigate to="/" />
+  // }
+  return (
     <section className="form-page">
-      <form onSubmit={handleSubmit} className="form">
-        <h1>Welcome back!</h1>
+      <form onSubmit={handleSubmit} className="form-card">
+        <h1 className="form-title">Welcome back!</h1>
         <br />
-
         {/* Email */}
         <div className="input-control">
-          <label htmlFor="email">Email</label>
+          <label htmlFor="email">Email:</label>
           <input type="email" name="email" id="email" placeholder="Email" required onChange={handleChange} value={formData.email} />
         </div>
 
         {/* Password */}
         <div className="input-control">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">Password:</label>
           <input type="password" name="password" id="password" placeholder="Password" required onChange={handleChange} value={formData.password} />
         </div>
 
-        { error.message && <p className="error-message">{error.message}</p> }
+        {error.message && <p className="error-message">{error.message}</p>}
 
-      <br />
-        <button className="SubmitButton" type="submit"> Signin</button> 
-      <br />
+        <button className="signin-submit-button" type="submit"> Sign In</button>
 
-        <small>Don't have an account? <Link to="/register">Register here</Link></small>
+        <p className="signin-footer-text">
+          Don't have an account? <Link to="/register">Register here</Link>
+        </p>
       </form>
     </section>
+
   )
 }
